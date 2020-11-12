4017f723ebe701405f18ed6416bb452f45d0fafc
김병곤 <fharenheit@gmail.com>
2020-11-13 07:46:04 +0900

    Maven POM 추가

---
+++ src/main/java/io/datadynamics/bigdata/spark/extension/App.java
@@ -0,0 +1,13 @@
+package io.datadynamics.bigdata.spark.extension;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
